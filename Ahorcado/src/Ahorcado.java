483f1701a318fb5b3758b63494fbb694ad7bd94c
Angel Faced Espada <angelfaced@hotmail.es>
2020-11-13 14:16:31 +0100

    Ahorcado mejorado

--- Ahorcado/src/Ahorcado.java
+++ Ahorcado/src/Ahorcado.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Ahorcado {
-
 	public static void main(String[] args) {
 		int vidas = 6;
 		int i = 0;
@@ -13,7 +12,7 @@ public class Ahorcado {
 		subpalabra2[1] = "";
 		subpalabra2[2] = "";
 		subpalabra2[3] = "";
-		String vLetra[] = new String[6];
+		String vLetra[] = new String[100];
 		subpalabra[0] = "h";
 		subpalabra[1] = "o";
 		subpalabra[2] = "l";
@@ -22,7 +21,7 @@ public class Ahorcado {
 		Scanner leer = new Scanner(System.in);
 			do {
 				System.out.println("dime una letra");
-				palabra2 = subpalabra2[0] + subpalabra2[1] + subpalabra2[2] + subpalabra[3];
+				
 				vLetra[i] = leer.next();
 				if (vLetra[i].equals(subpalabra[0]) || vLetra[i].equals(subpalabra[1]) || vLetra[i].equals(subpalabra[2]) || vLetra[i].equals(subpalabra[3]) ) {
 					System.out.println("la letra " + vLetra[i] + " esta en la palabra, tienes " + vidas + " vidas restantes");
@@ -30,13 +29,15 @@ public class Ahorcado {
 						subpalabra2[0] = vLetra[i];
 						System.out.println(vLetra[i]);
 						System.out.println("- - - -");
-		System.out.println(palabra2);
+						palabra2 = subpalabra2[0] + subpalabra2[1] + subpalabra2[2] + subpalabra2[3];
+						System.out.println(palabra2);
 						i=i+1;
 					} else {
 						if (vLetra[i].equals(subpalabra[1])) {
 							subpalabra2[1] = vLetra[i];
 							System.out.println("  " + vLetra[i]);
 							System.out.println("- - - -");
+							palabra2 = subpalabra2[0] + subpalabra2[1] + subpalabra2[2] + subpalabra2[3];
 							System.out.println(palabra2);
 							i=i+1;
 						} else {
@@ -44,6 +45,7 @@ public class Ahorcado {
 								subpalabra2[2] = vLetra[i];
 								System.out.println("    " + vLetra[i]);
 								System.out.println("- - - -");
+								palabra2 = subpalabra2[0] + subpalabra2[1] + subpalabra2[2] + subpalabra2[3];
 								System.out.println(palabra2);
 								i=i+1;
 							} else {
@@ -51,6 +53,7 @@ public class Ahorcado {
 									subpalabra2[3] = vLetra[i];
 									System.out.println("      " + vLetra[i]);
 									System.out.println("- - - -");
+									palabra2 = subpalabra2[0] + subpalabra2[1] + subpalabra2[2] + subpalabra2[3];
 									System.out.println(palabra2);
 									i=i+1;
 								}
@@ -61,19 +64,67 @@ public class Ahorcado {
 					
 				} else {
 					vidas = vidas - 1;
+					for (int j = 0; j < i; j++) {
+						if (vLetra[j].equals(vLetra[i])) {
+							System.out.println("Error la letra " + vLetra[i] + " esta repetida");
+						}
+					}
 					System.out.println("la letra " + vLetra[i] + " no esta en la palabra,  tienes " + vidas + " vidas restantes\"");
 					i = i+1;
 				}	
+				switch (vidas) {
+				case 1:
+					System.out.println("        -------");
+					System.out.println("        |     |");
+					System.out.println("        |     O ");
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("------------------");
+					break;
+				case 2:
+					System.out.println("        -------");
+					System.out.println("        |     |");
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("------------------");
+					break;
+				case 3:
+					System.out.println("        -------");
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("------------------");
+					break;
+				case 4:
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("        |      ");
+					System.out.println("------------------");
+					break;
+				case 5:
+					System.out.println("------------------");
+					break;
+				}
+
 			} while ( !palabra2.equals(palabra) && vidas != 0 );
-	
+			
 		if (vidas == 0) {
 			System.out.println("GAME OVER");
+			System.out.println("        -------");
+			System.out.println("        |     |");
+			System.out.println("        |     O ");
+			System.out.println("        |    /|l ");
+			System.out.println("        |    / 7");
+			System.out.println("        |      ");
+			System.out.println("------------------");
 		} else {
 			System.out.println("Felicidades has acertado la palabra secreta (" + palabra + ")");
 		}
 		
-			
-		
+
 	}
 
 }
